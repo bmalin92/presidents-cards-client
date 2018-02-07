e081f33e763078b3c226014738d60c862be5f4a8
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-02-07 15:03:12 -0800

    spinner added

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,10 +1,13 @@
-import {SET_AUTH_TOKEN, SET_CURRENT_USER, CLEAR_MESSAGE} from '../actions/auth';
+import {SET_AUTH_TOKEN, SET_CURRENT_USER, CLEAR_MESSAGE, 
+    AUTH_REQUEST, AUTH_SUCCESS, AUTH_ERROR} from '../actions/auth';
 import {DISPLAY_RESPONSE} from '../actions/users'; 
 
 const initialState = {
     authToken: null, // authToken !== null does not mean it has been validated
     currentUser: null, 
     message: null, 
+    loading: false,
+    error: null
 };
 
 export default function reducer(state = initialState, action) {
@@ -24,6 +27,20 @@ export default function reducer(state = initialState, action) {
         return Object.assign({}, state, {
             message: null
         })
+    } else if (action.type === AUTH_REQUEST) {
+        return Object.assign({}, state, {
+            loading: true,
+            error: null
+        });
+    } else if (action.type === AUTH_SUCCESS) {
+        return Object.assign({}, state, {
+            loading: false
+        });
+    } else if (action.type === AUTH_ERROR) {
+        return Object.assign({}, state, {
+            loading: false,
+            error: action.error
+        });
     }
     return state;
 }
